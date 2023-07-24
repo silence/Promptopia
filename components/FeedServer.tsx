78c64021a4c0f73ca2304fc5f36939e61f349506
zezhang.liu <zezhang.liu@shopee.com>
2023-07-25 00:59:11 +0800

    feat: add robust of server component

--- components/FeedServer.tsx
+++ components/FeedServer.tsx
@@ -2,20 +2,26 @@ import { PostWithUser } from "@types";
 import PromptCard from "./PromptCard";
 
 export const FeedServer = async () => {
-  const deployedUrl = process.env.NEXT_PUBLIC_VERCEL_URL;
+  const deployedUrl =
+    process.env.NEXT_PUBLIC_VERCEL_URL || "http://localhost:3000";
 
-  const res = await fetch(`${deployedUrl}/api/prompt`, {
-    next: {
-      revalidate: 10,
-    },
-  });
-  const posts: PostWithUser[] = await res.json();
+  try {
+    const res = await fetch(`${deployedUrl}/api/prompt`, {
+      next: {
+        revalidate: 10,
+      },
+    });
+    const posts: PostWithUser[] = await res.json();
 
-  return (
-    <div className="mt-16 prompt_layout">
-      {posts.map((post) => (
-        <PromptCard key={post._id} post={post} />
-      ))}
-    </div>
-  );
+    return (
+      <div className="mt-16 prompt_layout">
+        {posts.map((post) => (
+          <PromptCard key={post._id} post={post} />
+        ))}
+      </div>
+    );
+  } catch (err) {
+    console.log(err);
+    return <div>(Server) Something went wrong</div>;
+  }
 };
